bb017efc4b781357fad8ff02a1dd41bcf20f6120
KellyChess <chess.kelly@gmail.com>
2015-11-05 16:39:44 -0800

    Event scaffold added

---
+++ spec/views/events/edit.html.erb_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe "events/edit", type: :view do
+  before(:each) do
+    @event = assign(:event, Event.create!(
+      :title => "MyString",
+      :date => "MyString",
+      :location => "MyString",
+      :user => nil
+    ))
+  end
+
+  it "renders the edit event form" do
+    render
+
+    assert_select "form[action=?][method=?]", event_path(@event), "post" do
+
+      assert_select "input#event_title[name=?]", "event[title]"
+
+      assert_select "input#event_date[name=?]", "event[date]"
+
+      assert_select "input#event_location[name=?]", "event[location]"
+
+      assert_select "input#event_user_id[name=?]", "event[user_id]"
+    end
+  end
+end
---
+++ spec/views/events/new.html.erb_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe "events/new", type: :view do
+  before(:each) do
+    assign(:event, Event.new(
+      :title => "MyString",
+      :date => "MyString",
+      :location => "MyString",
+      :user => nil
+    ))
+  end
+
+  it "renders new event form" do
+    render
+
+    assert_select "form[action=?][method=?]", events_path, "post" do
+
+      assert_select "input#event_title[name=?]", "event[title]"
+
+      assert_select "input#event_date[name=?]", "event[date]"
+
+      assert_select "input#event_location[name=?]", "event[location]"
+
+      assert_select "input#event_user_id[name=?]", "event[user_id]"
+    end
+  end
+end
