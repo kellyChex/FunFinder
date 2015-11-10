87025d2a3234dc9566be5c95620195544c139379
Scott <spelo3@gmail.com>
2015-11-09 16:52:56 -0800

    Created event model testing and validation testing for Title, Location, Time, Date, User

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
   factory :event do
     title "MyString"
-date "MyString"
-location "MyString"
-time "2015-11-05 16:36:22"
-user nil
+    date "MyString"
+    location "MyString"
+    time "2015-11-05 16:36:22"
+    user nil
   end
 
 end
