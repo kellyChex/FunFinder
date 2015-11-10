7503cd207644f48179aa24842ed7e4ea62dae5f3
KellyChess <chess.kelly@gmail.com>
2015-11-10 15:54:52 -0800

    Added Paperclip to event

--- app/models/event.rb
+++ app/models/event.rb
@@ -10,6 +10,11 @@ class Event < ActiveRecord::Base
   validates :end_time, :presence => true
   validates :location, :presence => true
 
+  has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }
+  validates_attachment :image, presence: true,
+    content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] },
+    size: { in: 0..10.megabytes }
+
 
   private
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151110184405) do
+ActiveRecord::Schema.define(version: 20151110233446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,11 +21,15 @@ ActiveRecord::Schema.define(version: 20151110184405) do
     t.string   "location"
     t.time     "start_time"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.date     "start_date"
     t.date     "end_date"
     t.time     "end_time"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "events", ["user_id"], name: "index_events_on_user_id", using: :btree
