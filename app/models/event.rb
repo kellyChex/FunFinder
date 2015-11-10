989a367a4c6358bb1d198cea3a611e0a639421ee
Max Harvey <max_td44@yahoo.com>
2015-11-10 13:37:55 -0800

    Added start and end date for event table.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,26 +1,24 @@
 class Event < ActiveRecord::Base
-  validate do
-    #  errors[:start_date] << "cannot be in the past" if date && date < Date.today
-    #  errors[:end_date] << "cannot be past the start date" if date && date < start_date
-   end
+
 
   belongs_to :user
 
   validates :title, :presence => true
   validates :start_date, :end_date, :presence => true
   validate :end_after_start
-  validates :time, :presence => true
+  validates :start_time, :presence => true
+  validates :end_time, :presence => true
   validates :location, :presence => true
 
+
   private
+
   def end_after_start
     return if end_date.blank? || start_date.blank?
 
     if end_date < start_date
       errors.add(:end_date, "must be after the start date")
     end
-   end
-
-
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,23 +11,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151110080646) do
+ActiveRecord::Schema.define(version: 20151110184405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "end_times", force: :cascade do |t|
+    t.time "end_time"
+  end
+
   create_table "events", force: :cascade do |t|
     t.string   "title"
     t.string   "location"
-    t.time     "time"
+    t.time     "start_time"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date     "start_date"
     t.date     "end_date"
+    t.time     "end_time"
   end
 
   add_index "events", ["user_id"], name: "index_events_on_user_id", using: :btree
+
+  create_table "profiles", force: :cascade do |t|
+    t.string   "name"
+    t.text     "bio"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+    t.integer  "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
