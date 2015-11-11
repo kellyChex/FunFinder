aac5b9ed16e3888a7717081f841756743693ce5b
TylerLHenderson <tehendo@gmail.com>
2015-11-11 15:01:20 -0800

    Added js and view for google map api

---
+++ app/assets/javascripts/events.js
@@ -0,0 +1,8 @@
+alert("test")
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 32.7150, lng: -117.1625},
+    zoom: 13
+  });
+}
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,13 +11,10 @@ class User < ActiveRecord::Base
     validates_format_of :email,:with => Devise.email_regexp
 
     has_many :events
-    
+
     # accepts_nested_attributes_for :event
 
-    has_attached_file :image, styles: { thumb: "64x64", med: "100x100", large: "200x200" },
-    :default_url => "default_:style.png",
-    content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] },
-    size: { in: 0..10.megabytes }
+		has_attached_file :image, styles: { thumb: "64x64", med: "100x100", large: "200x200" }, :default_url => "default_:style.png", size: { in: 0..10.megabytes }
 
-
+		validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
