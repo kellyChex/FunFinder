98c8c472de6a050e6d7666cdcbb05e4dc045d39d
KellyChess <chess.kelly@gmail.com>
2015-11-11 14:46:06 -0800

    created event integration testing and created test for adding paperclip to event.

--- spec/features/add_image_to_event.rb
+++ spec/features/add_image_to_event.rb
@@ -1,12 +1,23 @@
 require 'rails_helper'
 
 describe "adding image to event" do
-  it "will allow users to upload picture to their profile" do
-    visit '/events'
-    fill_in('', :with => 'Iceland')
-    attach_file('Image', 'app/assets/images/headshot.png')
-    click_button('Create Profile')
-    page.should have_content('Kelly')
-    expect(page).to have_xpath("//img[contains(@src, 'headshot.png')]")
-  end
+ it "will allow users to upload picture to their profile" do
+   visit '/events'
+   click_link('New Event')
+   fill_in 'Title', :with => 'New Event'
+   attach_file 'event_image', '../FunFinder/spec/images/headshot.png'
+   fill_in 'Description', :with => 'myDescription'
+   select( '2015', :from => 'event_start_date_1i')
+   select( 'November', :from => 'event_start_date_2i')
+   select( '11', :from => 'event_start_date_3i')
+   select( '2015', :from => 'event_end_date_1i')
+   select( 'November', :from => 'event_end_date_2i')
+   select( '12', :from => 'event_end_date_3i')
+   fill_in 'Location', :with => 'San Diego'
+   fill_in 'event_start_time', :with => '00:45:00.000'
+   fill_in 'event_start_time', :with => '00:45:00.000'
+   fill_in 'event_end_time', :with => '13:09:00.000'
+   click_button('Create Event')
+   expect(page).to have_xpath("//img[contains(@src, 'headshot.png')]")
+ end
 end
--- spec/features/event_creation_spec.rb
+++ spec/features/event_creation_spec.rb
@@ -1,23 +1,26 @@
 require 'rails_helper'
 
-describe 'event creation', :type => :feature do
-  it 'should create an event' do
-    visit '/events'
-    click_link 'New Event'
-    fill_in 'Title', :with => 'New Event'
-    select( '2015', :from => 'event_start_date_1i')
-    select( 'November', :from => 'event_start_date_2i')
-    select( '10', :from => 'event_start_date_3i')
-    select( '2015', :from => 'event_end_date_1i')
-    select( 'November', :from => 'event_end_date_2i')
-    select( '11', :from => 'event_end_date_3i')
-    fill_in 'Location', :with => 'San Diego'
-    select( '10', :from => 'event_time_4i')
-    select( '00', :from => 'event_time_5i')
-    click_button 'Create Event'
 
-    expect(page).to have_content 'Event was successfully created.'
+describe 'event creation' do
+ it 'should create an event' do
+   visit '/events'
+   click_link('New Event')
+   fill_in 'Title', :with => 'New Event'
+   attach_file 'event_image', '../FunFinder/spec/images/headshot.png'
+   fill_in 'Description', :with => 'myDescription'
+   select( '2015', :from => 'event_start_date_1i')
+   select( 'November', :from => 'event_start_date_2i')
+   select( '11', :from => 'event_start_date_3i')
+   select( '2015', :from => 'event_end_date_1i')
+   select( 'November', :from => 'event_end_date_2i')
+   select( '12', :from => 'event_end_date_3i')
+   fill_in 'Location', :with => 'San Diego'
+   fill_in 'event_start_time', :with => '00:45:00.000'
+   fill_in 'event_start_time', :with => '00:45:00.000'
+   fill_in 'event_end_time', :with => '13:09:00.000'
+   click_button('Create Event')
+
+   expect(page).to have_content 'Event was successfully created.'
 
   end
-
-end
+ end
