241c85bff78f506a8bbff651b0477223ec7b2102
Tobias Brysiewicz <tbrysiewicz@att.net>
2015-11-11 15:03:28 -0800

    Fixed User Tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,10 +17,10 @@ class User < ActiveRecord::Base
     
     # accepts_nested_attributes_for :event
 
-    has_attached_file :image, styles: { thumb: "64x64", med: "100x100", large: "200x200" },
-    :default_url => "default_:style.png",
+has_attached_file :image, styles: { thumb: "64x64", med: "100x100", large: "200x200" },
+    :default_url => "default_:style.png"    
+    validates_attachment :image, 
     content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] },
     size: { in: 0..10.megabytes }
-
 
 end
