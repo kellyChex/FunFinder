584ec53b3080d838690fc06864677bc82df2a1ea
TylerLHenderson <tehendo@gmail.com>
2015-11-12 13:45:37 -0800

    Event map displays location of event

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,24 +1,10 @@
 $( document ).ready(function() {
   $("#event_location").geocomplete();
 
-  $("input[name=commit]").click(function(){
+  google.maps.event.addListener(autocomplete, 'place_changed', function(){
+    alert("listen")
     var place = autocomplete.getPlace();
-    document.getElementById('event_latitude').value = place.geometry.location.lat();
-    document.getElementById('event_longitude').value = place.geometry.location.lng();
+    $("#event_latitude").value = place.geometry.location.lat();
+    $("#event_longitude").value = place.geometry.location.lng();
   });
 });
-var directionsDisplay,
-    directionsService,
-    map;
-
-google.maps.event.addDomListener(window, "load", initMap);
-function initMap() {
-  var directionsService = new google.maps.DirectionsService();
-  directionsDisplay = new google.maps.DirectionsRenderer()
-  var latitude = document.getElementById("latitude").getAttribute("value");
-  var longitude = document.getElementById("longitude").getAttribute("value");
-  var location = new google.maps.LatLng(latitude,longitude);
-  var mapOptions = { zoom:13, center: location }
-  map = new google.maps.Map(document.getElementById("map"), mapOptions);
-  directionsDisplay.setMap(map);
-}
