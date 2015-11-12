15d0591b751fb6220abc2f1ca7e4c4365ad0acf7
TylerLHenderson <tehendo@gmail.com>
2015-11-11 20:00:57 -0800

    Map and Autocomplete work, but only after you refresh the page.

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,14 +1,12 @@
 $( document ).ready(function() {
-  $('#event_location').geocomplete();
-
-  
+  $("#event_location").geocomplete();
 });
 
-
-var map;
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
+  var map = new google.maps.Map(document.getElementById("map"), {
     center: {lat: 32.7150, lng: -117.1625},
     zoom: 13
   });
 }
+
+google.maps.event.addDomListener(window, "load", initMap);
