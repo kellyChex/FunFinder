6a6b3c25fb117c58e1f2d2fd7bd5f08529168efb
KellyChess <chess.kelly@gmail.com>
2015-11-13 12:20:46 -0800

    created model and features tests for tags.

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a name" do
+    aTag = Tag.new
+    aTag.name = 'myTag'
+    expect(aTag.name).to eq('myTag')
+  end
 end
