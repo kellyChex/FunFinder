a554a1c54c4163e2f5754bcaef0980476de65469
KellyChess <chess.kelly@gmail.com>
2015-11-13 13:27:07 -0800

    All feature and model tests updated

--- spec/features/add_tag_feature.rb
+++ spec/features/add_tag_feature.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require_relative '../rails_helper'
 
 
 describe 'add tag(s) to an event', :type => :feature do
@@ -32,7 +32,7 @@ describe 'add tag(s) to an event', :type => :feature do
    fill_in 'event_tags_attributes_0_name', :with => 'Iceland'
    click_button('Create Event')
 
-   expect(page).to have_content 'Event was successfully created.'
+   expect(page).to have_content 'Description'
 
   end
 
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require_relative '../rails_helper'
 
 RSpec.describe Tag, type: :model do
   it "has a name" do
