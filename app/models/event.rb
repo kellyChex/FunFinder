ed8dccd26f904992a91c3d90df47ab198ad71479
TylerLHenderson <tehendo@gmail.com>
2015-11-16 15:40:25 -0800

    Styled event show page, more work to be done

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,7 @@ class Event < ActiveRecord::Base
   has_many :tags, dependent: :destroy
   has_many :attendees, :class_name => 'Attendances', :foreign_key => 'user_id'
   has_many :attendance, :class_name => 'Attendances', :foreign_key => 'event_id'
-  has_attached_file :image, styles: { thumb: "64x64", med: "100x100", large: "300x300" }, dependent: :destroy, :default_url => "default_cal_:style.png"
+  has_attached_file :image, styles: { thumb: "64x64", med: "100x100", large: "300x300", xlarge: "1920x1080" }, dependent: :destroy, :default_url => "default_cal_:style.png"
 
   validates :title, :presence => true
   validates :description, :presence => true
@@ -13,7 +13,7 @@ class Event < ActiveRecord::Base
   validate :end_after_start
   validates :location, :presence => true
   validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }, size: { in: 0..10.megabytes }
- 
+
   geocoded_by :location
   after_validation :geocode, :if => :location_changed?
 
